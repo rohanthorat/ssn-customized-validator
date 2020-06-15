a8be38b5315cccecf5c4c4cdf0002770108d9f29
Rohan Thorat <rohanthorat@quickenloans.com>
2020-06-15 16:03:32 -0400

    adding spark input, error and reactive form control for ssn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SparkAngularModule } from '@sparkdesignsystem/spark-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SparkAngularModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
